3b69d0cda2f7d1763e5321ab93d19ac748d4d7d1
cauaoli-silva <cauafoxy@gmail.com>
2026-06-01 19:29:16 -0300

    JogoDaVelha.java

--- src/main/java/JogoDaVelha.java
+++ src/main/java/JogoDaVelha.java
@@ -1,26 +1,67 @@
-public static void main(String args[])
-{
-    Scanner sc = new Scanner(System.in);
+public class JogoDaVelha {
 
-    System.out.println("Nome: Cauã Oliveira Silva");
+    private Jogador jogador1;
+    private Jogador jogador2;
+    private Tabuleiro tabuleiro;
 
-    System.out.print("Escolha seu símbolo (X ou O): ");
+    public JogoDaVelha(Jogador jogador1, Jogador jogador2, Tabuleiro tabuleiro) {
+        this.jogador1 = jogador1;
+        this.jogador2 = jogador2;
+        this.tabuleiro = tabuleiro;
+    }
 
-    char simboloHumano = sc.next().toUpperCase().charAt(0);
+    public static void main(String[] args) {
 
-    char simboloMaquina = (simboloHumano == 'X') ? 'O' : 'X';
+        Scanner sc = new Scanner(System.in);
 
-    Jogador jogador1 = new JogadorHumano(simboloHumano);
+        System.out.println("Autor: Cauã Oliveira Silva");
+        System.out.print("Escolha seu símbolo (X ou O): ");
 
-    Jogador jogador2 = new JogadorMaquina(simboloMaquina);
+        char simboloHumano = sc.next().toUpperCase().charAt(0);
+        char simboloMaquina = (simboloHumano == 'X') ? 'O' : 'X';
 
-    Tabuleiro tabuleiro = new Tabuleiro();
+        Jogador jogador1 = new JogadorHumano(simboloHumano);
+        Jogador jogador2 = new JogadorMaquina(simboloMaquina);
 
-    JogoDaVelha jogoDaVelha;
+        Tabuleiro tabuleiro = new Tabuleiro();
 
-    jogoDaVelha = new JogoDaVelha(jogador1, jogador2, tabuleiro);
+        JogoDaVelha jogoDaVelha =
+                new JogoDaVelha(jogador1, jogador2, tabuleiro);
 
-    jogoDaVelha.iniciar();
+        jogoDaVelha.iniciar();
 
-    sc.close();
+        sc.close();
+    }
+
+    private void iniciar() {
+
+        int partida = 1;
+
+        while (!tabuleiro.acabouOJogo()) {
+
+            System.out.println("Partida " + partida);
+
+            jogador1.jogar(tabuleiro);
+
+            if (tabuleiro.acabouOJogo()) {
+                break;
+            }
+
+            jogador2.jogar(tabuleiro);
+
+            partida++;
+        }
+
+        if (tabuleiro.haUmVencedor()) {
+
+            if (partida % 2 == 1) {
+                System.out.println("O jogador 1 ganhou");
+            } else {
+                System.out.println("O jogador 2 ganhou");
+            }
+
+        } else {
+            System.out.println("O jogo terminou empatado.");
+        }
+    }
 }
